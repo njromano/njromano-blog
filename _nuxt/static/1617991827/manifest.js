32815737364b0ee802e2145702a748713e5ace66
Nick Romano <nromano@utk.edu>
2021-04-09 14:15:43 -0400

    Added publish command

---
+++ _nuxt/static/1617991827/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fabout","\u002Fprojects","\u002Fcontact","\u002Fblog","\u002Fblog\u002Fproductivity-tips-1","\u002Fblog\u002Ffirst-post","\u002Fblog\u002Farticle-ideas"]})
