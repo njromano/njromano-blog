a578761a0e245bc726801877014e27ae4491bd4d
Nick Romano <nromano@utk.edu>
2021-04-09 14:20:23 -0400

    Added publish command

--- dist/_nuxt/static/1617992313/contact/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1617992313",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcontact",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"67a723a7"}}}}(null));
