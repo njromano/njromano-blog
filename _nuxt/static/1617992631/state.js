118eb3d06896ad7da7b4859ebcd211e35e4517f2
Nick Romano <nromano@utk.edu>
2021-04-11 15:00:26 -0400

    run nuxt generate

--- _nuxt/static/1617992631/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1617992631",layout:"default",error:a,serverRendered:true,routePath:b,config:{app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"94810d3d"}}}}(null,"\u002F"));
